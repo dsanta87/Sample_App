00f575333df450889b33d761278fd1c7117d98df
David Santacruz <dsanta87@gmail.com>
2017-10-08 20:29:00 +0200

    exercises chapter 5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,10 +2,6 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
-  def setup
-    @static_title = "Ruby on Rails Tutorial Sample App"
-  end
-
   test "should get home" do
     get :home
     assert_response :success
